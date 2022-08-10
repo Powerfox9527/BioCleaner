c0e84eacb54548db4026d31e2e787befbe587a53
Powerfox9527 <2504488720@qq.com>
2022-08-10 20:36:51 +0800

    First Commit

---
+++ Assets/Scripts/Damageable.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface Damageable
+{
+    public abstract void GetHit(float damage);
+}
---
+++ Assets/Scripts/Zombie.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Zombie : MonoBehaviour, Damageable
+{
+    public GameObject loot;
+    public Player player;
+    public float HP = 100.0f;
+    public float Speed = 2.0f;
+    public CharacterController controller;
+    public AudioSource audioSource;
+    public AudioClip hitClip;
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player != null && !player.isDead)
+        {
+            Vector3 dir = player.transform.position - transform.position;
+            dir.y = 0;
+            dir.Normalize();
+            controller.Move(dir * Speed * Time.deltaTime);
+            transform.rotation = Quaternion.LookRotation(dir);
+        }
+    }
+
+    public void GetHit(float Damage) 
+    {
+        HP -= Damage; 
+        if(audioSource && hitClip)
+        {
+            audioSource.PlayOneShot(hitClip);
+        }
+        if(HP < 0)
+        {
+            if(loot)
+                Instantiate(loot);
+            Destroy(this.gameObject);
+        }
+    }
+}
